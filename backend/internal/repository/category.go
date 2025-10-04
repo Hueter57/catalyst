27f1372e92821ce0ede496d82dea3e3a6a6e187c
Hueter57 <kohsukemori@gmail.com>
2025-10-04 15:42:02 +0900

    feat: Categoryのメソッド枠作成

---
+++ backend/internal/repository/category.go
@@ -0,0 +1,28 @@
+package repository
+
+import "github.com/hueter57/catalyst/backend/internal/graph/model"
+
+func (r *Repository) GetCategoryByID(id string) (*model.Category, error) {
+	// TODO: implement
+	return nil, nil
+}
+
+func (r *Repository) GetCategoryList() ([]*model.Category, error) {
+	// TODO: implement
+	return nil, nil
+}
+
+func (r *Repository) CreateCategory(input model.CreateCategoryInput) (*model.Category, error) {
+	// TODO: implement
+	return nil, nil
+}
+
+func (r *Repository) UpdateCategory(input model.UpdateCategoryInput) (*model.Category, error) {
+	// TODO: implement
+	return nil, nil
+}
+
+func (r *Repository) DeleteCategory(id string) (bool, error) {
+	// TODO: implement
+	return false, nil
+}
--- backend/internal/service/category.go
+++ backend/internal/service/category.go
@@ -1,5 +1,11 @@
 package service
 
+import "github.com/hueter57/catalyst/backend/internal/graph/model"
+
 type CategoryRepository interface {
-	// Define repository methods needed by the service here
+	GetCategoryByID(id string) (*model.Category, error)
+	GetCategoryList() ([]*model.Category, error)
+	CreateCategory(input model.CreateCategoryInput) (*model.Category, error)
+	UpdateCategory(input model.UpdateCategoryInput) (*model.Category, error)
+	DeleteCategory(id string) (bool, error)
 }
